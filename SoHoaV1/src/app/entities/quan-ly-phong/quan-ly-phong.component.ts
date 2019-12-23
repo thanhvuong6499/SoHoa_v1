ae52d2fb81c4e37dc3d4c5acb602ee98f7299d2a
unknown <thanhvuong6499@gmail.com>
2019-12-24 00:24:21 +0700

    install ng bs

--- SoHoaV1/src/app/entities/quan-ly-phong/quan-ly-phong.component.ts
+++ SoHoaV1/src/app/entities/quan-ly-phong/quan-ly-phong.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ModalDirective, BsModalRef } from 'ngx-bootstrap';
-
+import {NgbModalRef} from '@ng-bootstrap/ng-bootstrap'
 @Component({
   selector: 'app-quan-ly-phong',
   templateUrl: './quan-ly-phong.component.html',
   styleUrls: ['./quan-ly-phong.component.css']
 })
 export class QuanLyPhongComponent implements OnInit {
-  private ngModalRef : BsModalRef;
+
   constructor(
     
   ) { }
