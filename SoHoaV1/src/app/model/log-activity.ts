cf55abc2255b7dafacf8d5b6d30cd8ad64957b7b
Nam <nhatnamdev98@gmail.com>
2020-08-27 11:14:26 +0700

    create log history function for document

--- SoHoaV1/src/app/model/log-activity.ts
+++ SoHoaV1/src/app/model/log-activity.ts
@@ -1,10 +1,11 @@
 
 export class LogActivity  {
     constructor(
-        public userName?: string,
-        public userID?: number,
-        public content?: string,
-        public profileNumber?: string,
+        public documentID?: number,
+        public documentCode?: string,
+        public logID?: number,
+        public docOrdinal?: string,
+        public description?: string,
         public creatorName?: string,
         public updatorName?: string,
         public createDate?: Date,
